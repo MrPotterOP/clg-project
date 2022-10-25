206501c6ce30f37f3cc22a1258a84cfdd035ced1
Shubham Ubarhande <shubham.ubarhande1044@gmail.com>
2022-10-25 16:23:49 +0530

    set schema required field

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,11 +2,12 @@ import mongoose from "mongoose";
 
 
 const userSchema = new mongoose.Schema({
-    "name": {require: true, type: String, max: 20},
-    "email": {require: true, type: String},
-    "password": {require: true, type: String, min: 6},
-    "institute": {require: true, type: String},
-    "verified": {type: Boolean, default: false}
+    "name": {required: true, type: String, max: 20},
+    "email": {required: true, type: String},
+    "password": {required: true, type: String, min: 6},
+    "institute": {required: true, type: String},
+    "verified": {type: Boolean, default: false},
+    "account": {type: String, default:"USER"}
 });
 
 const user = new mongoose.model("user", userSchema);

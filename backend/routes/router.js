bdeb2f475bb41ab83eead6c816aa05e7b8efaaf5
Shubham Ubarhande <shubham.ubarhande1044@gmail.com>
2022-12-02 12:40:49 +0530

    added newly created components routes

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,15 +1,17 @@
 import express from "express";
 
 import verifyUser from "../middleware/verifyUser.js";
+import checkUser from "../middleware/checkUser.js";
 
 import postUser from "../controllers/postUser.js";
 import postVerificationOTP from "../controllers/postVerificationOTP.js";
 import postGenerateOTP from "../controllers/postGenerateOTP.js";
 import postLogin from "../controllers/postLogin.js";
 import postTest from "../controllers/postTest.js";
-import checkUser from "../middleware/checkUser.js";
 import getInstitutes from "../controllers/getInstitutes.js";
 import getDashboard from "../controllers/getDashboard.js";
+import postQuestions from "../controllers/postQuestions.js";
+import patchTest from "../controllers/patchTest.js";
 
 const router = express.Router();
 
@@ -24,6 +26,8 @@ router.post("/login", postLogin);
 router.post("/verificationOTP", verifyUser, postVerificationOTP);
 router.post("/generateOTP", verifyUser, checkUser, postGenerateOTP);
 router.post("/createTest", verifyUser, checkUser, postTest);
+router.post("/addquestions", postQuestions);
+router.patch("/test", verifyUser, checkUser, patchTest);
 
 
 export default router;

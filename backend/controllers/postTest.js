1dd70862fd5689ca944473eddc648b5a8de202b6
Shubham Ubarhande <shubham.ubarhande1044@gmail.com>
2022-12-02 12:43:06 +0530

    created test creation route at backend api

--- backend/controllers/postTest.js
+++ backend/controllers/postTest.js
@@ -5,49 +5,64 @@ import queset from "../models/queset.js";
 
 const postTest = (req, res)=>{
     const {_id, verified, institute} = req.user;
-    const {name, description, duration, date, time, questions} = req.body;
+    const {name, description, duration, date, time, questions, level} = req.body;
+    var list = [];
 
 
-    const addQueset = (id)=>{
 
-        const deleteTest = ()=>{
-            test.findOneAndDelete({_id: id}, (err, result)=>{
-                if(err){
-                    console.log(err);
-                }else{
-                    console.log(result);
-                }
-            });
-        }
+    //Functional Components
 
-        queset.create({_id: id, questions}, (err, doc)=>{
-            if(err){
-                deleteTest();
-                return res.status(501).json({msg: "something went wrong."})
-            }else if(!doc){
-                deleteTest();
-                return res.status(401).json({msg: "Provide Valid Information."});
-            }else{
-                return res.status(200).json({msg: "Test Sheduled."})
-            }
-        })
+    const pushList = (x)=>{
+        list.push(x._id);
     }
 
     const createTest = ()=>{
-        const scheduleData = {
-            institute: _id, name, description, duration, date, time
-        }
-
-        test.create({...scheduleData}, (err, doc)=>{
+        
+        queset.aggregate([{
+                "$match": {"level" : level}
+            },
+            {"$sample" : {"size": questions}}
+        ]).exec((err, queSet) => {
             if(err){
-                return res.status(501).json({msg: "something went wrong"})
-            }else if(!doc){
-                return res.status(401).json({msg: "Provide Valid Information."});
+                return res.status(402).json({msg: "Input valid questions defficulty."})
             }else{
-                addQueset(doc._id);
+                queSet.forEach(x =>{
+                    pushList(x);
+                })
+                sheduleTest(list);
             }
         });
     }
+
+    const sheduleTest = (data) =>{
+        //date [day] [month] [year]
+        const sheduleDate = date.split("/");
+        
+        //time [hours] [min]
+        const sheduleTime = time.split("/");
+
+        if(sheduleDate.length != 3 || sheduleTime.length != 2 || sheduleDate[0] < 0 || sheduleDate[1] < 0 || sheduleDate[2] < 0 || sheduleDate[0] > 31 || sheduleDate[1] > 12 || sheduleDate[2] < 2022 || sheduleTime[0] < 0 || sheduleTime[0] > 23 || sheduleTime[1] < 0 || sheduleTime[1] > 59){
+            return res.status(406).json({msg: "Provide valide date and time."});
+        }else{
+            const completeDate = {
+                    day: parseInt(sheduleDate[0]),
+                    month: parseInt(sheduleDate[1]),
+                    year: parseInt(sheduleDate[2]),
+                    time: {hour:parseInt(sheduleTime[0]), min: parseInt(sheduleTime[1])}
+            }
+            test.create({name, description, duration, questions: data, date: completeDate, institute: _id}, (err, doc)=>{
+                if(err){
+                    return res.status(400).json({msg: err});
+                }else if(!doc){
+                    return res.status(406).json({msg: "Not Implimented."});
+                }else{
+                    return res.json({msg: "Test Sheduled"});
+                }
+            })
+        }
+        
+    }
+
 
 
     if(!verified){
@@ -60,9 +75,7 @@ const postTest = (req, res)=>{
             return res.status(401).json({msg: "Provide Valid Data"});
         }else{
             createTest();
-
         }
-
     }
 }
 

177a7bd2b3ff0becbaa323e0fa6bb6032bd6aaec
Shubham Ubarhande <shubham.ubarhande1044@gmail.com>
2022-12-02 12:43:27 +0530

    updated quesets model added score field

--- backend/models/queset.js
+++ backend/models/queset.js
@@ -4,12 +4,10 @@ import mongoose from "mongoose";
 
 
 const quesetSchema = new mongoose.Schema({
-    _id: {type: mongoose.Schema.Types.ObjectId, required: true},
-    questions: [{
-        question: {type: String, min: 4, max: 200, required: true},
-        options: [{type: String, required: true}],
-        answer: {type: Number, min: 1, max: 4, required: true}
-    }]
+    question: {type: String, min: 4, max: 200, required: true},
+    options: [{type: String, required: true}],
+    answer: {type: Number, min: 0, max: 3, required: true},
+    level: {type: String, required: true, default: "Easy"}
 });
 
 const queset = new mongoose.model("queset", quesetSchema);

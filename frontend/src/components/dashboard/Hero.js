dae7bb6fd0c61f18a6e847a2be3f7c5a3d1debc2
Shubham Ubarhande <shubham.ubarhande1044@gmail.com>
2022-12-07 13:00:16 +0530

    added different bg to hero for test route

--- frontend/src/components/dashboard/Hero.js
+++ frontend/src/components/dashboard/Hero.js
@@ -1,11 +1,20 @@
 import React from "react";
 
 
-const Hero = ()=>{
+const Hero = ({img})=>{
+
+    let url;
+
+    if(!img){
+        url = "/images/bg-art.jpg";
+    }else{
+        url = `/images/${img}.jpg`;
+    }
+
     return(
         <section className="section-hero">
             <div className="hero-img-container">
-                <div className="hero-bg-img" style={{"background": `url("/images/bg-art.jpg")`}}></div>
+                <div className="hero-bg-img" style={{"background": `url(${url})`}}></div>
                 <p>यद्यदाचरति श्रेष्ठस्तत्तदेवेतरो जन:। स यत्प्रमाणं कुरुते लोकस्तदनुवर्तते॥</p>
             </div>
         </section>
